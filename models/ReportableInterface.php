a87648213c0bdb666daa9c027db7facbb1503b07
Jadranko <jakrajinovic@gmail.com>
2018-06-02 00:24:43 +0200

    Add: reports logic added

---
+++ models/ReportableInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jadro
+ * Date: 2.6.2018.
+ * Time: 0:04
+ */
+
+namespace app\models;
+
+
+interface ReportableInterface
+{
+
+}
---
+++ models/Reports.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jadro
+ * Date: 1.6.2018.
+ * Time: 22:44
+ */
+
+namespace app\models;
+
+
+class Reports
+{
+
+    public function informationFrom($battle)
+    {
+            return 'rezultati';
+    }
+}
