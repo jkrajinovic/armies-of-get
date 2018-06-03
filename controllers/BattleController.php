384cbff35e1a84f84d5e55e083d5efe005cdbb2d
Jadranko <jakrajinovic@gmail.com>
2018-06-03 22:06:14 +0200

    Remove: unnecessary behaviours

--- controllers/BattleController.php
+++ controllers/BattleController.php
@@ -1,42 +1,14 @@
 <?php
-declare(strict_types=1);
 
 namespace app\controllers;
 
-use app\services\BattleService;
+
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
+use app\services\BattleService;
 
 class BattleController extends Controller
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
 
     /**
      * Displays homepage.
@@ -45,7 +17,6 @@ class BattleController extends Controller
      */
     public function actionIndex()
     {
-
         $report = BattleService::engage(20,20);
         return $this->render('index', compact('report'));
     }
